3e95196686081d831ff236f3884183a46e7a461a
surya29 <surya.v1315137@ksrce.ac.in>
2016-07-20 14:51:39 +0530

    Create vowels.java

---
+++ vowels.java
@@ -0,0 +1,18 @@
+
+public class vowels {
+
+
+	public static void main(String[] args) {
+			char c;
+			c=args[0].charAt(0);
+			if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+			{
+				System.out.println("vowels");
+			}
+			else
+			{
+				System.out.println("consonents");
+			}
+	}
+
+}
