703a800f6f73da61fb61aa17ff9700cf512cfa30
surya29 <surya.v1315137@ksrce.ac.in>
2016-07-20 11:41:38 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,21 @@
+public class pos {
+
+	
+	public static void main(String[] args) {
+		
+     int i=Integer.parseInt(args[0]);
+     if(i==0)
+     {
+    	 System.out.println("The number is zero");
+     }
+     else if(i>0)
+     {
+    	 System.out.println("The number is poitive");
+     }
+     else
+     {
+    	 System.out.println("The number is negative");
+     }
+	}
+
+}
