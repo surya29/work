7034bfb41b2beaab53a81ab03f29bd26775030f7
surya29 <surya.v1315137@ksrce.ac.in>
2016-08-08 01:59:58 -0700

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,27 @@
+
+public class Swap {
+
+	public static String swapPairs(String s) {
+	    String even = "";
+	    String odd = "";
+	    int length = s.length();
+
+	    for (int i = 0; i <= length-2; i+=2) {          
+	        even += s.charAt(i+1) + "" + s.charAt(i);
+	    }
+
+	    if (length % 2 != 0) {          
+	        odd = even + s.charAt(length-1);
+	        return odd;
+	    } else {
+	        return even;
+	    }
+	    
+	}
+	public static void main(String[] args) {
+		
+		swapPairs("hello world");
+		
+	}
+
+}
